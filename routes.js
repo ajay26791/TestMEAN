2aae801a4bf1b01a3ca3507c06a98529b8e01fba
ShriGanesh <AjayGouda@ShriGaneshs-MacBook-Pro.local>
2017-01-15 00:56:01 +0530

    Test

---
+++ routes.js
@@ -0,0 +1,39 @@
+var express=require("express");
+var app=express();
+var db=require("monk")("45.112.250.108:27017/TestDB");
+var user=db.get("user");
+
+
+var responseUserList=function(req,res){
+    user.find({},function(err, userList){
+        if (err) throw err;
+      	res.json(userList);
+    });
+};
+
+var responseRegUser=function(req,res){
+   
+    var json = '';
+    req.on('data', function (data) {
+        json += data;
+    });
+
+    req.on('end', function () {
+        console.log("UserInsert: "+json);
+        user.insert(JSON.parse(json),function(err, userList){
+            if (err) throw res.send(err);
+            res.json(userList);
+        });
+    });
+
+};
+
+app.get("/userList",responseUserList);
+
+app.post("/registerUser",responseRegUser)
+
+app.listen('3000',function(){
+    console.log("Listening port 3000");
+});
+
+
